9bd7885d9a3f1f7783d9a2dbf9dd85bd67f621ff
Daniel Seminara <japaneseforgaming@gmail.com>
2023-01-07 16:28:13 +0000

    Saved progress on ThrowCustomException 2023-01-07 16:28:13 +0000

--- ThrowCustomException/src/main/java/CustomExceptionThrower.java
+++ ThrowCustomException/src/main/java/CustomExceptionThrower.java
@@ -8,7 +8,13 @@ public class CustomExceptionThrower {
      *
      * I recommend looking at the test case for a good example of try/catch block usage, as well.
      */
-    public void throwCustomException(){
+    public void throwCustomException() throws CustomException{
+        try{
+            int a = 3/0;
+        } catch(Exception e) {
+            throw new CustomException("khl");
+        }
+
 
     }
 
